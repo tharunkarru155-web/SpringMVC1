0dd4f964806d2a0b0920ff78ea54777cdbb4e059
tharunkarru155-web <tharunkarru155@gmail.com>
2026-09-04 15:16:47 +0530

    Update UserController.java
    2nd change

--- src/main/java/com/ashok/controller/UserController.java
+++ src/main/java/com/ashok/controller/UserController.java
@@ -14,7 +14,7 @@ public class UserController {
 	@RequestMapping(value = "/displayForm.htm", method = RequestMethod.GET)
 	public String displayForm(Model model) {
 		System.out.println("hi");
-		System.out.println("hello");
+		//System.out.println("hello");
 		int i = 10;
 		model.addAttribute("user", new User());
 		return "userForm";
